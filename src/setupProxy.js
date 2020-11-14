1c4deceb10bdded187c386a8c365908b7555910a
zimprod23 <zimproduction23@gmail.com>
2020-11-14 22:44:24 +0100

    heroku2

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://git.heroku.com/zimstore.git",
+      target: "https://zimstore.herokuapp.com",
       changeOrigin: true,
     })
   );
