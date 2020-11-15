4e178e023963b06ccd87be218ef5f4678ee80004
zimprod23 <58174863+zimprod23@users.noreply.github.com>
2020-11-15 22:03:34 +0100

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://zimstore.herokuapp.com",
+      target: "https://zimstore.herokuapp.com/",
       changeOrigin: true,
     })
   );
